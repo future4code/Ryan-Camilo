4703c97ef79f020cdc69c9b63a6a0c7498a7e25a
Ryan Camilo <ryancamilo@hotmail.com>
2021-06-15 17:08:10 -0300

    Concluido exercício 06

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -1,51 +1,78 @@
 // EXERCÍCIO 01
 function inverteArray(array) {
-const reversedArray = []
-  for (let i=array.length-1; i>=0; i--){
+  const reversedArray = []
+  for (let i = array.length - 1; i >= 0; i--) {
     reversedArray.push(array[i])
   }
-return reversedArray
+  return reversedArray
 }
 
 // EXERCÍCIO 02
 function retornaNumerosParesElevadosADois(array) {
-  const filtrarPar = array.filter((inputArray)=>{
-    return inputArray%2 === 0
+  const filtrarPar = array.filter((inputArray) => {
+    return inputArray % 2 === 0
   })
-  const elevarPar = filtrarPar.map((inputArray)=>{
-    return inputArray**2
+  const elevarPar = filtrarPar.map((inputArray) => {
+    return inputArray ** 2
   })
   return elevarPar
 }
 
 // EXERCÍCIO 03
 function retornaNumerosPares(array) {
-  const filtrarPar2 = array.filter((inputArray)=>{
-    return inputArray%2 === 0
+  const filtrarPar2 = array.filter((inputArray) => {
+    return inputArray % 2 === 0
   })
   return filtrarPar2
 }
 
 // EXERCÍCIO 04
 function retornaMaiorNumero(array) {
-let maior = 0
-  array.forEach((element,index) => {
-    if (maior < array[index]){
+  let maior = 0
+  array.forEach((element, index) => {
+    if (maior < array[index]) {
       maior = element
     }
   });
-return maior
+  return maior
 }
 
 // EXERCÍCIO 05
 function retornaQuantidadeElementos(array) {
-  return array.length
-  
+  return array.length    // Ué? kkkkkk
+
 }
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
-
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2
+  const booleano4 = !booleano3
+  let respostas = []
+  respostas.length = 5
+  for (let i=0; i<respostas.length; i++){
+    switch (i) {
+      case 0:
+        respostas[i]=(booleano1 && booleano2 && !booleano4)
+        break;
+      case 1:
+        respostas[i]=((booleano1 && booleano2) || !booleano3)
+        break;
+      case 2:
+        respostas[i]=(booleano2 || booleano3) && (booleano4 || booleano1)
+        break;
+      case 3:
+        respostas[i]=(!(booleano2 && booleano3) || !(booleano1 && booleano3))
+        break;
+      case 4:
+        respostas[i]=(!(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3))
+        break;
+      default:
+        break;
+    }
+  }
+  return respostas
 }
 
 // EXERCÍCIO 07
